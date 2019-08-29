0af239d701c0902dc52c3afe23a7d089d9331c44
sabsrolz <sabsrolz@umich.edu>
2019-08-29 14:20:26 -0500

    declared initial function for AJAX method

--- assets/api.js
+++ assets/api.js
@@ -1,34 +1,44 @@
 $(document).ready(function() {});
-const key = "e68dd11c5f272a6460046d68e988be4d";
-queryURL = "https://developers.zomato.com/api/v2.1/locations?query=Chicago";
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-  // crossDomain: true,
-  // dataType: "json",
-  headers: {
-    "user-key": key,
-    Accept: "application/json"
-  }
-}).then(function(response) {
-  console.log(response);
+function displayRestaurants() {
+  let city = $("#city-input")
+    .val()
+    .trim();
+  let max_results = 5;
+
+  const key = "e68dd11c5f272a6460046d68e988be4d";
+  queryURL = `https://developers.zomato.com/api/v2.1/locations?query=${city}&count=${max_results}`;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+      "user-key": key,
+      Accept: "application/json"
+    }
+  }).then(function(response) {
+    let location_type = response.location_suggestions[0].entity_type;
+    let city_id = response.location_suggestions[0].city_id;
+    console.log(location_type);
+    console.log(city_id);
+    $.ajax({
+      url: `https://developers.zomato.com/api/v2.1/location_details?entity_id=${city_id}&entity_type=${location_type}`,
+      method: "GET",
+      headers: {
+        "user-key": key,
+        Accept: "application/json"
+      }
+    }).then(function(response) {
+      let best_rated = response.best_rated_restaurant;
+      console.log(best_rated);
+      let res_name = best_rated[0].restaurant.name;
+      console.log(res_name);
+    });
+  });
+}
+
+$("#add-city").on("click", function(event) {
+  event.preventDefault();
+
+  displayRestaurants();
 });
-
-// const queryURL =
-//   "https://webcamstravel.p.rapidapi.com/webcams/list/country=GT&show=webcams:image,location,url";
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-//   headers: {
-//     "x-rapidapi-host": "webcamstravel.p.rapidapi.com",
-//     "x-rapidapi-key": "c04ea25392msh10e8fa8f87c46a4p1e4622jsnf4d70b746bed"
-//   }
-// }).then(function(response) {
-//   console.log(response);
-//   web_url = JSON.stringify(response.result.webcams[0].url.edit);
-//   console.log(web_url);
-
-//   $("#image_div").replaceWith(`<img src =${web_url}/>`);
-// });
+//displayRestaurants();
